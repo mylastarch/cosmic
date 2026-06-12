67599bf670fc93dd6d12607d90866fa9bb93ab2e
mylastarch <mylastarch@dccathome.com>
2026-06-12 13:55:56 -0400

    update

---
+++ cosmic.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+##################################################################################################################################
+# Author    : Dan Cihon 
+# Website   : https://www.dccwebsite.com
+# Youtube   : https://youtube.com/future
+##################################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################################
+
+set -Euo pipefail
+shopt -s nullglob
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+COMMON_DIR="$(cd -- "${SCRIPT_DIR}/../common" && pwd)"
+
+source "${COMMON_DIR}/common.sh"
+
+echo
+tput setaf 2
+echo "########################################################################"
+echo "################### Install Cosmic"
+echo "########################################################################"
+tput sgr0
+echo
+
+list=(
+plasma
+kde-system-meta
+ark
+breeze
+cryfs
+discover
+dolphin
+dolphin-plugins
+encfs
+ffmpegthumbs
+gocryptfs
+gwenview
+kate
+kde-gtk-config
+kdeconnect
+kdenetwork-filesharing
+ktorrent
+#ocs-url
+okular
+packagekit-qt6
+partitionmanager
+spectacle
+#surfn-plasma-dark-icons-git
+yakuake
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+    count=$[count+1]
+    tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+    install_packages $name
+done
+
+echo
+tput setaf 3
+echo "########################################################################"
+echo "SKEL"
+echo "Copying all files and folders from /etc/skel/ to ~"
+echo "########################################################################"
+tput sgr0
+echo
+
+echo
+tput setaf 6
+echo "##############################################################"
+echo "###################  $(basename $0) done"
+echo "##############################################################"
+tput sgr0
+echo
